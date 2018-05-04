d6cc22416670f9bfc521bd5e8a4ed2e3690e6880
Adrian Likins <alikins@redhat.com>
2018-05-04 18:48:56 -0400

    fix test_content_name
    was using another modules parse_content_name

--- tests/ansible_galaxy/utils/test_content_name.py
+++ tests/ansible_galaxy/utils/test_content_name.py
@@ -1,24 +1,24 @@
 
-from ansible_galaxy.flat_rest_api import content
+from ansible_galaxy.utils import content_name
 
 import logging
 log = logging.getLogger(__name__)
 
 
 def test_parse_content_name():
-    content_name = 'alikins.testing-content'
+    _content_name = 'alikins.testing-content'
 
-    user_name, repo_name, content_name = content.parse_content_name(content_name)
+    user_name, repo_name, _content_name = content_name.parse_content_name(_content_name)
 
     assert user_name == 'alikins'
-    assert content_name == 'testing-content'
+    assert _content_name == 'testing-content'
     assert repo_name is None
 
-    content_name = 'alikins.testing-content.elasticsearch_plugin.py'
-    user_name, repo_name, content_name = content.parse_content_name(content_name)
+    _content_name = 'alikins.testing-content.elasticsearch_plugin.py'
+    user_name, repo_name, _content_name = content_name.parse_content_name(_content_name)
 
     assert user_name == 'alikins'
-    assert content_name == 'elasticsearch_plugin.py'
+    assert _content_name == 'elasticsearch_plugin.py'
     assert repo_name == 'testing-content'
 
     # TODO: revisit rules for content name /spec, ie, can
