0dc54476cbdf1bb763978abf22fd391c1f088277
Adrian Likins <alikins@redhat.com>
2018-05-07 17:32:06 -0400

    rm custom akl logger bits
    didn't mean to commit that

--- ansible_galaxy_cli/logger/setup.py
+++ ansible_galaxy_cli/logger/setup.py
@@ -4,15 +4,11 @@ import logging
 import logging.config
 import os
 
-try:
-    import color_debug
-except ImportError as e:
-    print(e)
-
 LOG_FILE = os.path.expandvars(os.path.expanduser('~/.ansible/ansible-galaxy-cli.log')),
 
-#DEFAULT_LEVEL = 'INFO'
 DEFAULT_LEVEL = 'DEBUG'
+DEFAULT_DEBUG_FORMAT = '[%(asctime)s,%(msecs)03d %(process)05d %(levelname)-0.1s] %(name)s %(funcName)s:%(lineno)d - %(message)s'
+DEFAULT_HANDLERS = ['console', 'file']
 
 DEFAULT_LOGGING_CONFIG = {
     'version': 1,
@@ -22,12 +18,8 @@ DEFAULT_LOGGING_CONFIG = {
     'formatters': {
         # skip the date for console log handler, but include it for the file log handler
         'console_verbose': {
-            'class': color_debug.ColorFormatter,
-            'format': '[%(asctime)s,%(msecs)03d %(process)05d %(levelname)-0.1s] %(name)s %(funcName)s:%(lineno)d - %(message)s',
-            'fmt': '[%(asctime)s,%(msecs)03d %(process)05d %(levelname)-0.1s] %(name)s %(funcName)s:%(lineno)d - %(message)s',
+            'format': DEFAULT_DEBUG_FORMAT,
             'datefmt': '%H:%M:%S',
-            'default_color_by_attr': 'name',
-            'auto_color': True
         },
         'file_verbose': {
             'format': '[%(asctime)s %(process)05d %(levelname)-0.1s] %(name)s %(funcName)s:%(lineno)d - %(message)s',
@@ -53,11 +45,11 @@ DEFAULT_LOGGING_CONFIG = {
 
     'loggers': {
         'ansible_galaxy': {
-            # 'handlers': ['console', 'file'],
+            'handlers': DEFAULT_HANDLERS,
             'level': DEFAULT_LEVEL,
         },
         'ansible_galaxy_cli': {
-            # 'handlers': ['console', 'file'],
+            'handlers': DEFAULT_HANDLERS,
             'level': DEFAULT_LEVEL,
         },
     }
@@ -69,19 +61,9 @@ def setup(logging_config=None):
 
     conf = logging.config.dictConfig(logging_config)
 
-    log = logging.getLogger(__name__)
-
-    #root_logger = logging.getLogger('')
-    #root_handler = logging.StreamHandler()
-    #formatter = color_debug.ColorFormatter()
-    #root_handler.setFormatter(formatter)
-    #root_logger.addHandler(root_handler)
-
-    from akl import alogging
-    log = alogging.default_setup('')
-    # log.debug('foo')
     # import logging_tree
     # logging_tree.printout()
+
     return conf
 
 
