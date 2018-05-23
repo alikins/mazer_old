8c3f6b39e2478c96a9221b20eda28c3f523fd09b
Adrian Likins <alikins@redhat.com>
2018-05-23 17:58:44 -0400

    wip, cleanup flake8/style

--- tests/ansible_galaxy_cli/cli/test_galaxy_upstream.py
+++ tests/ansible_galaxy_cli/cli/test_galaxy_upstream.py
@@ -44,10 +44,6 @@ from ansible_galaxy import exceptions
 
 from ansible_galaxy_cli.cli.galaxy import GalaxyCLI
 from ansible_galaxy_cli import exceptions as cli_exceptions
-# from ansible.compat.tests import unittest
-# from ansible.compat.tests.mock import call, patch
-# from ansible.errors import AnsibleError, AnsibleOptionsError
-# from ansible.module_utils.six import PY3
 
 log = logging.getLogger(__name__)
 
